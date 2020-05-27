be28f22e0fa0e291b05be567bf80812f4e7b56ea
jssaninv <jssaninv@eafit.edu.co>
2020-05-27 18:42:19 -0500

    error23

--- measure/views.py
+++ measure/views.py
@@ -8,10 +8,12 @@ def measure(request):
     # Verifica si hay un parametro value en la petición GET
     if 'value' in request.GET:
         value = request.GET['value']
+        latitud=request.GET['latitud']
+        longitud=request.GET['longitud']
         # Verifica si el value no esta vacio
         if value:
             # Crea el json para realizar la petición POST al Web Service
-            args = {'type': 'ph', 'value': value}
+            args = {'type': 'ph', 'value': value,'latitud':latitud,'longitud':longitud}
            # response = requests.post('http://127.0.0.1:8000/measuressssss/', args)
             response = requests.post('http://pi1-eafit-jssaninv.azurewebsites.net/measuressssss/',args)
             # Convierte la respuesta en JSON
